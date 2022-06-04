7bf1b7f4807c5464e90fed04cbb3dbf491ce0534
MarvinLaptop <marvin.k.schrading@gmail.com>
2022-06-04 17:12:07 +0200

    Added octaves layers to the noise generation

--- Assets/Scripts/HeigtMapGenerator.cs
+++ Assets/Scripts/HeigtMapGenerator.cs
@@ -5,34 +5,42 @@ using UnityEngine;
 public class HeigtMapGenerator : MonoBehaviour
 {
     [Header("Map Values")]
-    [SerializeField]
-    private int mapWidth = 0;
-    [SerializeField]
-    private int mapHeigth = 0;
-    [SerializeField]
-    private float noiseScale = 0f;
+    [SerializeField] private int mapWidth = 0;
+    [SerializeField] private int mapHeigth = 0;
+    [SerializeField] private float noiseScale = 0f;
+    [SerializeField] private int octaves = 0;
+    
+    [Range(0,1)]
+    [SerializeField] private float persistance = 0.5f;
+    [SerializeField] private float lacunarity = 2f;
 
+    [SerializeField] private int seed;
+    [SerializeField] private Vector2 offset;
+    
     public bool autoUpdateMap = false;
 
 
 
     public void GenerateMap()
     {
-        float[,] noiseMap = NoiseGenerator.GenerateNoiseMap(mapWidth, mapHeigth, noiseScale);
+        float[,] noiseMap = NoiseGenerator.GenerateNoiseMap(mapWidth, mapHeigth, seed, noiseScale ,octaves, persistance, lacunarity, offset);
         
         MapPlaneDisplayer mapDisplay = FindObjectOfType<MapPlaneDisplayer>();
         
         mapDisplay.DrawNoiseMasp(noiseMap);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnValidate() {
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+        if (mapHeigth < 1) {
+            mapHeigth = 1;
+        }
+        if (lacunarity < 1) {
+            lacunarity = 1;
+        }
+        if (octaves < 0) {
+            octaves = 0;
+        }
     }
 }
--- Assets/Scripts/NoiseGenerator.cs
+++ Assets/Scripts/NoiseGenerator.cs
@@ -7,29 +7,61 @@ using UnityEngine;
 // This class handle the generation of noise in order to be used to create various HeigtMaps
 public class NoiseGenerator  
 {
-   public static float[,] GenerateNoiseMap(int witdh, int heigth, float scale)
-   {
-      // 2D array
-      float[,] noiseMap = new float[witdh, heigth];
+   public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+      float[,] noiseMap = new float[mapWidth,mapHeight];
 
-      if (scale <= 0)
-      {
+      // Use the seed to generate the same map regarding the given seed
+      System.Random prng = new System.Random (seed);
+      // Give an offset to each octave in order to sample them from random different locations
+      Vector2[] octaveOffsets = new Vector2[octaves];
+      for (int i = 0; i < octaves; i++) {
+         float offsetX = prng.Next (-100000, 100000) + offset.x;
+         float offsetY = prng.Next (-100000, 100000) + offset.y;
+         octaveOffsets [i] = new Vector2 (offsetX, offsetY);
+      }
+
+      if (scale <= 0) {
          scale = 0.0001f;
       }
 
-      for (int y = 0; y < heigth; ++y)
-      {
-         for (int x = 0; x < witdh; ++x)
-         {
-            // divide by scale to get non integer value that will not be the same everytime
-            float sampleX = x / scale;
-            float sampleY = y / scale;
-            
-            // Get perlin value for the map point
-            float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+      float maxNoiseHeight = float.MinValue;
+      float minNoiseHeight = float.MaxValue;
 
-            noiseMap[x, y] = perlinValue;
+      // Calculate the half witdh and half heigth in order to zoom in the center of the map instead of into the corner
+      float halfWidth = mapWidth / 2f;
+      float halfHeight = mapHeight / 2f;
 
+
+      for (int y = 0; y < mapHeight; y++) {
+         for (int x = 0; x < mapWidth; x++) {
+		
+            float amplitude = 1;
+            float frequency = 1;
+            float noiseHeight = 0;
+
+            for (int i = 0; i < octaves; i++) {
+               float sampleX = (x-halfWidth) / scale * frequency + octaveOffsets[i].x;
+               float sampleY = (y-halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+               float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
+               noiseHeight += perlinValue * amplitude;
+
+               amplitude *= persistance;
+               frequency *= lacunarity;
+            }
+
+            if (noiseHeight > maxNoiseHeight) {
+               maxNoiseHeight = noiseHeight;
+            } else if (noiseHeight < minNoiseHeight) {
+               minNoiseHeight = noiseHeight;
+            }
+            noiseMap [x, y] = noiseHeight;
+         }
+      }
+
+      for (int y = 0; y < mapHeight; y++) {
+         for (int x = 0; x < mapWidth; x++) {
+            noiseMap [x, y] = Mathf.InverseLerp (minNoiseHeight, maxNoiseHeight, noiseMap [x, y]);
          }
       }
 
